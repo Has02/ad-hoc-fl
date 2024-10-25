53ac2f75ad43c7e849d7e0e1f5ad3d5f04636b9c
nrvadlamudi <nrv434@utexas.edu>
2024-10-25 16:20:31 -0500

    speed selection script

---
+++ battery.py
@@ -0,0 +1,14 @@
+"""
+Distribution of initial battery life and battery life reduction functions for devices.
+For each device, we will select a random phone model and a random initial battery life.
+After each communication round, we will reduce battery life based on the training time and average power consumption of the phone.
+
+"""
+
+import numpy as np
+
+
+
+"""
+Initialize phone and battery life.
+"""
